d9a8458a8b1ac4bb79a5433e14beb466c0b86c5d
Leo14-glitch <nala18349@gmail.com>
2021-10-09 21:05:02 +0800

    updated Main.java

--- src/prelim/Main.java
+++ src/prelim/Main.java
@@ -27,15 +27,37 @@ public class Main {
 
     NestedList<NestedList<sluPortal>> sluPortalList = new NestedList<>();
     NestedList<sluPortal> homeMenu = new NestedList<>();
-    sluPortal home = new sluPortal("Home", null);
-    sluPortal announcement = new sluPortal("Events, News and Announcement", "DRAGONPAY - SLU Payment Procedures -view attachment");
-    sluPortal studStatus = new sluPortal("Student Status", "Status:\n" +
-            "- CURRENTLY ENROLLED THIS FIRST SEMESTER, 2021-2022 IN BSCS 2.");
-
-    //Schedule
     NestedList<sluPortal> schedMenu = new NestedList<>();
-    sluPortal schedule = new sluPortal("Schedule", null);
-    sluPortal classSchedule = new sluPortal("Class Schedule (FIRST SEMESTER, 2021-2022)", """
+    NestedList<sluPortal> attendanceMenu = new NestedList<>();
+    NestedList<sluPortal> statementOfAccMenu = new NestedList<>();
+    //TODO grd
+    //TODO trns of rec
+    NestedList<sluPortal> journalMenu = new NestedList<>();
+    //TODO curri check
+    NestedList<sluPortal> schoolCalendarMenu = new NestedList<>();
+    NestedList<sluPortal> perDet = new NestedList<>();
+    NestedList<sluPortal> downAbout = new NestedList<>();
+    NestedList<sluPortal> ptuRes = new NestedList<>();
+
+    public static void main(String[] args) {
+        Main obj = new Main();
+        obj.run();
+
+    }
+
+    private void run(){
+        // home menu
+        sluPortal home = new sluPortal("Home", null);
+        sluPortal announcement = new sluPortal("Events, News and Announcement", "DRAGONPAY - SLU Payment Procedures -view attachment");
+        sluPortal studStatus = new sluPortal("Student Status", "Status:\n" +
+                "- CURRENTLY ENROLLED THIS FIRST SEMESTER, 2021-2022 IN BSCS 2.");
+        homeMenu.insert(home);
+        homeMenu.insert(announcement);
+        homeMenu.insert(studStatus);
+        sluPortalList.insert(homeMenu);
+        // schedule
+        sluPortal schedule = new sluPortal("Schedule", null);
+        sluPortal classSchedule = new sluPortal("Class Schedule (FIRST SEMESTER, 2021-2022)", """
             Class Code      Course Number       Course Description                  Units      Schedule             Days    Room\n
             7025	        NSTP-CWTS 1	        FOUNDATIONS OF SERVICE	            3	        0730 - 1200 PM	    FS	    BATCH2\n
             7897	        FIT OA	            PHYSICAL ACTIVITY                   2	        0730 - 1030 AM	    WTH	    BATCH2\n
@@ -49,53 +71,62 @@ public class Main {
             9424	        GSTS	            SCIENCE, TECHNOLOGY, AND SOCIETY	3	        0730 - 1200 PM	    MT	    BATCH1\n
             9425	        GRVA	            READING VISUAL ART	                3	        0730 - 1200 PM	    MT	    BATCH3\n
             9426	        CFE 103	            CATHOLIC FOUNDATION OF MISSION	    3	        0430 - 0530 PM	    MWF	    SEMESTRAL\n""");
-    sluPortal totalUnits = new sluPortal("23", null);
+        sluPortal totalUnits = new sluPortal("23", null);
+        schedMenu.insert(schedule);
+        schedMenu.insert(classSchedule);
+        schedMenu.insert(totalUnits);
+        //  attendance
+        sluPortal attendance = new sluPortal("Attendance", null);
+        sluPortal studBehaviour = new sluPortal("Student Behavior","Bahaviour:\n"+"Great! No Absences/Tardiness were found.");
+        attendanceMenu.insert(attendance);
+        attendanceMenu.insert(studBehaviour);
 
-    //Attendance Menu
-    NestedList<sluPortal> attendanceMenu = new NestedList<>();
-    sluPortal attendance = new sluPortal("Attendance", null);
-    sluPortal studBehaviour = new sluPortal("Student Behavior","Bahaviour:\n"+"Great! No Absences/Tardiness were found.");
-    
-    //Statement Of Account
-    NestedList<sluPortal> statementOfAccMenu = new NestedList<>();
-    sluPortal statementOfAcc = new sluPortal("Statement of Account", null);
-    sluPortal firstSemStatOfAcc = new sluPortal ("Statement of Accounts (FIRST SEMESTER, 2021-2022)", """
+        //  stt of acc
+        sluPortal statementOfAcc = new sluPortal("Statement of Account", null);
+        sluPortal firstSemStatOfAcc = new sluPortal ("Statement of Accounts (FIRST SEMESTER, 2021-2022)", """
             Your amount due for PRELIM is:\s
             P X,XXX.XX\s
             Your Remaining Balance as of October 2021 is:\s
             P XX,XXX.XX""");
-    sluPortal mode = new sluPortal("Mode of Payment", "Dragonpay \n" + "Pay with Bukas");
-    sluPortal brkOfFees = new sluPortal("Breakdown of fees as of October 2021", "Date, Description, Amount");
-    sluPortal payTransacs = new sluPortal ("Dragonpay Payment Transactions (FIRST SEMESTER, 2021-2022)", "Date, Channel, Reference, Amount");
-
-    //TODO grd
-    //TODO trns of rec
-    //Journal
-    NestedList<sluPortal> journalMenu = new NestedList<>();
-    sluPortal journal = new sluPortal("Journal", null);
-    sluPortal journalIndexes = new sluPortal("Saint Louis University Libraries Periodical Article Indexes", null);
-    sluPortal whatJournalIndexes = new sluPortal("WHAT ARE JOURNAL INDEXES?", """
+        sluPortal mode = new sluPortal("Mode of Payment", "Dragonpay \n" + "Pay with Bukas");
+        sluPortal brkOfFees = new sluPortal("Breakdown of fees as of October 2021", "Date, Description, Amount");
+        sluPortal payTransacs = new sluPortal ("Dragonpay Payment Transactions (FIRST SEMESTER, 2021-2022)", "Date, Channel, Reference, Amount");
+        statementOfAccMenu.insert(statementOfAcc);
+        statementOfAccMenu.insert(firstSemStatOfAcc);
+        statementOfAccMenu.insert(mode);
+        statementOfAccMenu.insert(brkOfFees);
+        statementOfAccMenu.insert(payTransacs);
+        //  grd
+        //  trns of rec
+        //  journ
+        sluPortal journal = new sluPortal("Journal", null);
+        sluPortal journalIndexes = new sluPortal("Saint Louis University Libraries Periodical Article Indexes", null);
+        sluPortal whatJournalIndexes = new sluPortal("WHAT ARE JOURNAL INDEXES?", """
                                                     An index is a list of items pulled together for a purpose. Journal indexes also\s
                                                     called bibliographic indexes or bibliographic databases are lists of journals,\s 
                                                     organized by discipline, subject, or type of publication.""");
-    sluPortal periodicalLibrariesIndexes = new sluPortal("THE SLU LIBRARIES’ PERIODICAL ARTICLE INDEXES", """
+        sluPortal periodicalLibrariesIndexes = new sluPortal("THE SLU LIBRARIES’ PERIODICAL ARTICLE INDEXES", """
                                                     One of the Home Library Services that the University Libraries offer\s 
                                                     is the Periodical Article Indexes where the subscribed print journals\s 
                                                     are being indexed and can be accessed through an online bibliographic database.\s
 
                                                     The Periodical Article Indexes database provides access to periodical articles\s 
                                                     by subject or author and it can help you find articles about a specific topic.""");
-    sluPortal accessingArticleIndexes = new sluPortal("STEPS IN ACCESSING THE PERIODICAL ARTICLE INDEXES", """
+        sluPortal accessingArticleIndexes = new sluPortal("STEPS IN ACCESSING THE PERIODICAL ARTICLE INDEXES", """
                                                             1. Enter your topic on the search box and click Search\s
                                                             2. You will see the various bibliographic details (i.e. title of the journal, the specific date,\s 
                                                                 volume and issue, and page numbers for the article) that contain your topic.\s
                                                             3. Should you opt to read the full text of the article,\s 
                                                                 you may request it by sending an email to uldir@slu.edu.ph""");
-    //TODO curri check
-    //School Calendar
-    NestedList<sluPortal> schoolCalendarMenu = new NestedList<>();
-    sluPortal schoolCalendar = new sluPortal("School Calendar", null);
-    sluPortal calendar = new sluPortal("School Calendar SY 2021-2022\n", "At present date and time is: " + cal.getTime() + """
+        journalMenu.insert(journal);
+        journalMenu.insert(journalIndexes);
+        journalMenu.insert(whatJournalIndexes);
+        journalMenu.insert(periodicalLibrariesIndexes);
+        journalMenu.insert(accessingArticleIndexes);
+        //  curri check
+        //  schl clndr
+        sluPortal schoolCalendar = new sluPortal("School Calendar", null);
+        sluPortal calendar = new sluPortal("School Calendar SY 2021-2022\n", "At present date and time is: " + cal.getTime() + """
                                             \nGeneral Events:\n
                                                 FIRST SEMESTER, 2021-2022\n
                                                 Registration (No Classes)	August 09, 2021 Monday\n
@@ -110,83 +141,36 @@ public class Main {
                                             Others:\n
                                                 No events to display!\n
                                             """);
-
-    NestedList<sluPortal> perDet = new NestedList<>();
-    sluPortal perD = new sluPortal("Personal Test Developement", null);
-    sluPortal genInfo = new sluPortal("General Information", "Gender:Male\n Birthday:September 30, 2000\n" +
-                                           "Religion: Roman Catholic\n Birthplace:Baguio City\n Nationality: ");
-    sluPortal conInfo = new sluPortal("Contact Information", "Home Address:Null\n Home Telephone Number:Null\n" +
-                                          "Baguio Address:Null\n Cellphone Number:Null\n Email:juandealcruz@gmail.com");
-    sluPortal conPersons = new sluPortal("Contact Persons", "Father:N/A\n Occupation:N/A\n Mother:N/A\n");
-   
-   // Downloadables/ About SLU
-                                                 
-   NestedList<sluPortal> downAbout = new NestedList<>();
-    sluPortal downloadAbout = new sluPortal("Downloadables & About SLU", null);
-    sluPortal downloadables = new sluPortal("Downloadables","<1>Student Handbok\n<2>Safety Orientation Manual\n" +
-                                            "<3>Ordinance 021-2018 - Harassement\n<4>Gsuite For Education\n<5>The Mission and Identity Cluster");
-    sluPortal aboutSLU = new sluPortal("About iSLU Portal","iSLU portal is for saint Louis university students and parents, that serves as a personal assistant in carrying out " + 
-                                      "university related tasks.");
-                                               
-                                                                                       
-                                                 
-                                                 
-    NestedList<sluPortal> ptuRes = new NestedList<>();
-    sluPortal ptu = new sluPortal("PTU Test Result", null);
-    sluPortal slucee = new sluPortal("SLU-COLLEGE ENTRANCE EXAMINATION (SLU-CEE) RESULTS", "No Results found");
-    sluPortal sosr = new sluPortal("STUDY ORIENTATION SURVEY-REVISED (SOS-R) TEST RESULTS", "No Results found");
-    sluPortal picrs = new sluPortal("POSITIVE INTERNAL COPING RESOURCES SCALES (PICRS) TEST RESULTS", "No Results found");
-
-    public static void main(String[] args) {
-        Main obj = new Main();
-        obj.run();
-
-    }
-
-    private void run(){
-        // home menu
-        homeMenu.insert(home);
-        homeMenu.insert(announcement);
-        homeMenu.insert(studStatus);
-        sluPortalList.insert(homeMenu);
-        // schedule
-        schedMenu.insert(schedule);
-        schedMenu.insert(classSchedule);
-        schedMenu.insert(totalUnits);
-        //  attendance
-        attendanceMenu.insert(attendance);
-        attendanceMenu.insert(studBehaviour);
-        //  stt of acc
-        statementOfAccMenu.insert(statementOfAcc);
-        statementOfAccMenu.insert(firstSemStatOfAcc);
-        statementOfAccMenu.insert(mode);
-        statementOfAccMenu.insert(brkOfFees);
-        statementOfAccMenu.insert(payTransacs);
-        //  grd
-        //  trns of rec
-        //  journ
-        journalMenu.insert(journal);
-        journalMenu.insert(journalIndexes);
-        journalMenu.insert(whatJournalIndexes);
-        journalMenu.insert(periodicalLibrariesIndexes);
-        journalMenu.insert(accessingArticleIndexes);
-        //  curri check
-        //  schl clndr
         schoolCalendarMenu.insert(schoolCalendar);
         schoolCalendarMenu.insert(calendar);
         
        // Personal Details
+        sluPortal perD = new sluPortal("Personal Test Developement", null);
+        sluPortal genInfo = new sluPortal("General Information", "Gender:Male\n Birthday:September 30, 2000\n" +
+                "Religion: Roman Catholic\n Birthplace:Baguio City\n Nationality: ");
+        sluPortal conInfo = new sluPortal("Contact Information", "Home Address:Null\n Home Telephone Number:Null\n" +
+                "Baguio Address:Null\n Cellphone Number:Null\n Email:juandealcruz@gmail.com");
+        sluPortal conPersons = new sluPortal("Contact Persons", "Father:N/A\n Occupation:N/A\n Mother:N/A\n");
         perDet.insert(perD);
         perDet.insert(genInfo);
         perDet.insert(conInfo);
         perDet.insert(conPersons);
         
         // Downloadables & About iSLU
+        sluPortal downloadAbout = new sluPortal("Downloadables & About SLU", null);
+        sluPortal downloadables = new sluPortal("Downloadables","<1>Student Handbok\n<2>Safety Orientation Manual\n" +
+                "<3>Ordinance 021-2018 - Harassement\n<4>Gsuite For Education\n<5>The Mission and Identity Cluster");
+        sluPortal aboutSLU = new sluPortal("About iSLU Portal","iSLU portal is for saint Louis university students and parents, that serves as a personal assistant in carrying out " +
+                "university related tasks.");
         downAbout.insert(downloadAbout);
         downAbout.insert(downloadables);
         downAbout.insert(aboutSLU);
         
         //PTU test result
+        sluPortal ptu = new sluPortal("PTU Test Result", null);
+        sluPortal slucee = new sluPortal("SLU-COLLEGE ENTRANCE EXAMINATION (SLU-CEE) RESULTS", "No Results found");
+        sluPortal sosr = new sluPortal("STUDY ORIENTATION SURVEY-REVISED (SOS-R) TEST RESULTS", "No Results found");
+        sluPortal picrs = new sluPortal("POSITIVE INTERNAL COPING RESOURCES SCALES (PICRS) TEST RESULTS", "No Results found");
         ptuRes.insert(ptu);
         ptuRes.insert(slucee);
         ptuRes.insert(sosr);
